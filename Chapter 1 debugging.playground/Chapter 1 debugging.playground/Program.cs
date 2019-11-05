fa14eaa133f354d66be96e002b06d2ca197aebbe
jonvernon16 <57112508+jonvernon16@users.noreply.github.com>
2019-11-05 13:54:08 -0600

    commiting chapter 1

---
+++ Chapter 1 debugging.playground/Chapter 1 debugging.playground/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_1_debugging.playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CCCCC   #  #");
+            Console.WriteLine("C     ########");
+            Console.WriteLine("C       #  #");
+            Console.WriteLine("C     ########");
+            Console.WriteLine("CCCCC   #  #");
+            Console.WriteLine("Take Highway 51 north");
+            Console.WriteLine("Exit at County Road H");
+            Console.WriteLine("Go three blocks and make a right on School Street");
+            Console.WriteLine("Go right at Elm");
+            Console.WriteLine("Party is four houses down on the left");
+            Console.WriteLine();
+            Console.WriteLine("Spinach dip:");
+            Console.WriteLine();
+            Console.WriteLine("1 package frozen chopped spinach");
+            Console.WriteLine("1 cup mayonaise");
+            Console.WriteLine("1 chopped onion");
+            Console.WriteLine();
+            Console.WriteLine("Mix ingredients together.");
+            Console.WriteLine("Chill and serve with chips.");
+        }
+    }
+}
